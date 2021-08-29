444db74094e595cc42024b7668a4c197ef96a442
rookieProgrammer1999 <saiphaneesh123@gmail.com>
2021-08-29 07:46:20 +0530

    ProGrad ID

--- src/app.js
+++ src/app.js
@@ -2,27 +2,50 @@
 //We've got some basic info about Karen's home
 //Debug the type of data provided
 //Return the types concatenated in a single variable
-
+function moreAboutHome(address, distanceFromTown, hasNeighbours) {
+    // console.log(address + distanceFromTown + hasNeighbours);
+    return typeof address + typeof distanceFromTown + typeof hasNeighbours
+}
 //Progression 2:
 //Check if the data given is of the right type
 //parents = String, noOfSiblings = Number, isNuclearFamily = Boolean
-
+function moreAboutKaren(parents, noOfSiblings, isNuclearFamily) {
+    // if (typeof parents == "string") return true
+    // if (typeof noOfSiblings == "Number") return true
+    // if (typeof isNuclearFamily == "Boolean") return true
+    // if (typeof parents + typeof noOfSiblings + typeof isNuclearFamily == "stringNumberBoolean") return true
+    // if (typeof parents === undefined && typeof noOfSiblings === undefined && typeof isNuclearFamily === undefined) return false
+    // return false
+    return typeof parents == "string" &&
+        typeof noOfSiblings == "number" &&
+        typeof isNuclearFamily == "boolean";
+}
 //Progression 3:
 //Lily is suspicious about Karen's new friend
 //Karen tells her friend's age and even writes it down
 //Check which one those is not a number (NaN) and return that value
-
+function doesFriendExist(ageInText, ageInNumber) {
+    return ageInText == "NaN" ? "NaN" : isNaN(ageInText) ? ageInText : ageInNumber
+}
 //Progression 4:
 //Lily gave Karen x sweets
 //Karen ate y sweets herself
 //On her way to the river, she ate another z sweets every n meters travelled
 //Her friend divided the remaining sweets into 2 parts for each
 //How many sweets did her friend get to eat?
-
+function sweetTooth(totalNoOfSweets, sweetsConsumedByKaren, sweetsConsumedInNMeters, metersToTravel) {
+    // let sweetsWithKarenFriendLeft;
+    if (isNaN(totalNoOfSweets) || isNaN(sweetsConsumedInNMeters) || isNaN(sweetsConsumedByKaren) || isNaN(metersToTravel)) return `No sweets for Karen's friend`
+        // if (sweetsConsumedByKaren == totalNoOfSweets) return 0
+    return (totalNoOfSweets - sweetsConsumedByKaren - sweetsConsumedInNMeters * metersToTravel) / 2
+}
 //Progression 5:
 //As Lily moves closer, it gets colder. She checks the temperature on her mobile
 //It only shows in fahrenheit. Convert the data to celsius and return it.
-
+function convertToCelsius(fahrenheit) {
+    if (typeof fahrenheit == "undefined" || typeof fahrenheit == "string" || typeof fahrenheit == "object") return 'Technical Error!'
+    return 5 / 9 * (fahrenheit - 32)
+}
 //Progression 6:
 //Lily can now do multiple things to deal with this
 //1. Take her daughter to a doctor
@@ -30,7 +53,22 @@
 //3. Counsel her daughter herself
 //4. Lock her daughter in her room
 //Given a value, return which of these above actions Lily would take
-
+function aDifficultChoice(choice) {
+    if (typeof choice == "undefined") return `Wasn't able to decide`
+    switch (choice) {
+        case 1:
+            return 'Take her daughter to a doctor';
+            break;
+        case -1:
+            return 'Break down and give up all hope';
+            // break;
+        case 'I give up':
+            return 'Refused to do anything for Karen';
+            break;
+        default:
+            break;
+    }
+}
 //Progression 7:
 //Lily realized that she'd hurt her daughter
 //All she wants now is for her to stop crying
--- src/link.js
+++ src/link.js
@@ -8,6 +8,7 @@ var nextString = [
     "Haunting discovery",
     "Rising paranoia"
 ];
+
 function updateProgression(i, p) {
     filler.innerHTML = "Task: Complete Trial " + (i + 1) + "<br>Next: " + nextString[i];
     if (story.lastElementChild == filler)
@@ -56,7 +57,7 @@ if (moreAboutHome("Riverside Manor", 25, false) == "stringnumberboolean") {
         }
     }
 }
-document.getElementById("link").addEventListener("click", function (e) {
+document.getElementById("link").addEventListener("click", function(e) {
     e.preventDefault();
     let theEnd = "<p>Lily rushed to Harold and told him what happened. Harold tried to console her and told her everything would be alright. Lily pleaded Harold, \"Won't you please take our daughter to a doctor? She said the word \'kill\'. Something is definitely wrong with her.\"</p><p>Harold ignored her statement and asked her to calm down. This only made Lily more angry. She started shouting at Harold, \"You won't let us move to the town close by! You won't take our daughter to the hospital. You don't love us! You never did!\"</p><p>Harold snapped when he heard that. He broke down, \"The problem isn't with our daughter. It's with you! Karen died 6 months ago. You and Karen were on a boat ride when she drowned in the river. THERE IS NO KAREN. THERE IS NO FRIEND. It's all in your head.\"</p><p>Lily listened to everything Harold had to say and couldn't believe what she was hearing. She started laughing hysterically and had lost herself. She fell unconcious Harold stood there crying to himself.</p><p>--THE END--</p>";
     story.innerHTML += theEnd;
